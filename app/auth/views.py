50046a221001b765f8985f5879078d721f309499
kingsam91 <lambetgm@gmail.com>
2020-07-20 04:39:14 +0300

    define login/logout/register routes

--- app/auth/views.py
+++ app/auth/views.py
@@ -23,4 +23,22 @@ def register():
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('home.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Pitches login"
+    return render_template('auth/login.html', login_form=login_form, title=title)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been successfully logged out')
+    return redirect(url_for("home.index"))
