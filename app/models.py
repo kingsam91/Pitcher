094748b0c496373e64a95d35e8c8c8773b393482
kingsam91 <lambetgm@gmail.com>
2020-07-20 04:12:55 +0300

    setup login

--- app/models.py
+++ app/models.py
@@ -70,5 +70,47 @@ class Category(db.Model):
             'created_at': self.created_at
         }
 
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    upvotes = db.Column(db.Integer, default=0)
+    downvotes = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    comments = db.relationship(
+        'Comment', backref='pitch', lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls):
+        rows = Pitch.query.all()
+        return rows
+
+    @classmethod
+    def get_my_pitches(cls, id):
+        rows = Pitch.query.filter_by(user_id=id).all()
+        return rows
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    pitch_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return f'User {self.name}'
+
 
 
